1e6447eb42ef7173692d8b6c1db63b5e2e13f165
Cyril VINH-TUNG <cyril@invitu.com>
2025-12-24 19:01:55 -1000

    [MIG] partner_title_for_company: Migration to 18.0

--- partner_title_for_company/__manifest__.py
+++ partner_title_for_company/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Partner title for companies",
     "summary": "Add titles for companies",
-    "version": "17.0.1.1.0",
+    "version": "18.0.1.0.0",
     "author": "INVITU, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "maintainer": "INVITU",
